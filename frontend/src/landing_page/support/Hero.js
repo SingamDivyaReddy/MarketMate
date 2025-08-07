bdd45d8b6ff512681e4cf962325b9a881c27f5ca
SingamDivyaReddy <singamdivya04@gmail.com>
2025-08-07 15:22:41 +0530

    Changes done in hero section

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -3,23 +3,26 @@ import React from "react";
 function Hero() {
   return (
     <section className="container-fluid" id="supportHero">
-      <div className="p-5 " id="supportWrapper">
-        <h4>Support Portal</h4>
-        <a href="google">Track Tickets</a>
-      </div>
-      <div className="row p-5 m-3">
-        <div className="col-6 p-3">
-          <h1 className="fs-3">
-            Search for an answer or browse help topics to create a ticket
-          </h1>
+
+      <div className="row p-3 m-3">
+        <div className="col-12 p-3">
+          <div style={{
+            display: 'flex',
+            justifyContent: 'space-between', // Pushes children to opposite ends
+            alignItems: 'center',            // Vertically aligns them in the middle
+            padding: '10px',                 // Adds some nice spacing   // A border to see the container's bounds
+          }}>
+            <h1>Support Portal</h1>
+            <a href="google">Track Tickets</a>
+          </div>
           <input placeholder="Eg. how do I activate F&O" />
           <br />
-          <a href="google">Track account opening</a>
-          <a href="google">Track segment activation</a>
-          <a href="google">Intraday margins</a>
-          <a href="google">Kite user manual</a>
         </div>
-        <div className="col-6 p-3">
+
+      </div>
+      <div className="row p-3 m-3">
+        <div className="col-1 p-3"></div>
+        <div className="col-5 p-3">
           <h1 className="fs-3">Featured</h1>
           <ol>
             <li>
@@ -28,6 +31,16 @@ function Hero() {
             <li>
               <a href="google">Latest Intraday leverages - MIS & CO</a>
             </li>
+          </ol>
+        </div>
+        <div className="col-2 p-3"></div>
+        <div className="col-4 p-3">
+          <h1 className="fs-3">Quick links</h1>
+          <ol>
+            <li><a href="google">Track account opening</a></li>
+            <li><a href="google">Track segment activation</a></li>
+            <li><a href="google">Intraday margins</a></li>
+            <li><a href="google">Kite user manual</a></li>
           </ol>
         </div>
       </div>
